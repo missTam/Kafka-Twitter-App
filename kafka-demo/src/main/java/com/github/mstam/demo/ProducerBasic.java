7009324149cf481fc98dc1900dd88b14ffede44a
msTam <ttac@netlight.com>
2021-09-04 18:14:23 +0200

    Code refactor

---
+++ kafka-demo/src/main/java/com/github/mstam/demo/ProducerBasic.java
@@ -0,0 +1,40 @@
+package com.github.mstam.demo;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class ProducerBasic {
+    public static void main(String[] args) {
+
+        String bootsrapServers = "127.0.0.1:9092";
+
+        // Define producer configs
+        Properties properties = new Properties();
+
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootsrapServers);
+        // what type of value is being sent to kafka and how it should be serialized (converted) into bytes
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create producer with key (extends) String & value as String
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        // create producer record
+        // @param topic: where is the record being published?
+        // @param value: what is being published?
+        ProducerRecord<String, String> record = new ProducerRecord<>("first_topic", "hello kafka");
+
+        // publish data - async
+        producer.send(record);
+
+        // wait for data to be produced
+        // flush data
+        producer.flush();
+        // close producer
+        producer.close();
+    }
+}
